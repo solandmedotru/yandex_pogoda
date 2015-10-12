6061f5165028db3ac75ab3c78cc7919332a4038f
Andrey Solomin <solomin.andrey@gmail.com>
2015-10-12 23:42:10 +0300

    added city id select

--- pogoda.rb
+++ pogoda.rb
@@ -6,14 +6,26 @@ require 'date'
 # <city id="27581" region="11156" head="0" type="2" country="Россия" part="Чувашия" resort="0" climate="">Чебоксары</city>
 # Формат ссылки export.yandex.ru/weather-ng/forecasts/27581.xml
 
-uri = URI.parse('http://export.yandex.ru/weather-ng/forecasts/27581.xml')
+puts "Введите ID города и нажмите Enter. По умолчанию будет 27581 (Чебоксары, Россия)."
+
+def city
+  city_id = gets.chomp.to_i
+  if city_id == 0
+    27581
+    else city_id
+  end
+end
+
+
+
+uri = URI.parse("http://export.yandex.ru/weather-ng/forecasts/#{city}.xml")
 
 response = Net::HTTP.get_response(uri)
 
 doc = REXML::Document.new(response.body)
 
 city_name = doc.root.attributes['exactname']
-time = DateTime.now
+time = Time.now
 observation_time = doc.root.elements['fact/observation_time'].text
 temperature = doc.root.elements['fact/temperature'].text
 pogoda = doc.root.elements['fact/weather_type'].text
@@ -21,10 +33,10 @@ wind = doc.root.elements['fact/wind_speed'].text
 
 puts "Сейчас #{time.strftime("%d/%m/%Y %k:%M")}, погода в городе #{city_name}"
 puts "#{temperature} градус(ов), #{pogoda}, ветер #{wind} м/с по данным на #{observation_time}"
-
+puts
 doc.root.elements.each('day') do |element|
   puts 'Прогноз на'
-  puts element.attributes['date']
+  puts DateTime.parse(element.attributes['date']).strftime("%d/%m/%Y")
   puts "----------------------"
 
   element.elements.each('day_part') do |element|
